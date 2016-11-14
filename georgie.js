4a4173243e08e0cddc217ed7c6fd087822a85e9a
Geordan <geo22joseph@gmail.com>
2016-11-13 19:33:43 -0500

    features added
    can now add more bathrooms and it will check if you have the points to
    add them.
    you can now level up and the points to level up to the next level is
    100^1+(.5*level) this will most likely change

--- georgie.js
+++ georgie.js
@@ -1,11 +1,20 @@
 
 //var cookieString = document.cookie;
 var totalClickAmount = 0;
-
+var luxuryBathroomClick;
+var level = 1;
+var levelPointsLeft = 0;
+var levelMult = 1;
+var amountOfBathrooms = 0;
 
 //function to handle all clicks
 function clickOnGeorge(clicks){
   totalClickAmount = totalClickAmount + clicks;
+  //checks to see if the player should level up because of this click
+  if(totalClickAmount - (Math.pow(100, levelMult)) >= 0){
+    levelUp();
+    console.log("test");
+  }
   //document.cookie = "totalClickAmount = " + totalClickAmount;
   console.log(totalClickAmount);
 }
@@ -13,8 +22,29 @@ function clickOnGeorge(clicks){
 //a powerup to help click george automatically
 //5 clicks a second per luxuryBathroomSpots activated
 //20 is the most you can have activated
-function luxuryBathroomSpots(amountOfBathrooms){
-  if(amountOfBathrooms != 0 && amountOfBathrooms < 20){
-    setInterval(clickOnGeorge(5), 1000/amountOfBathrooms);
+function luxuryBathroomSpots(amountOfBathroomsInput){
+  amountOfBathrooms = amountOfBathrooms + amountOfBathroomsInput;
+  if(amountOfBathrooms > 0 && amountOfBathrooms < 20 && levelPointsLeft >= 50){
+    clearInterval(luxuryBathroomClick);
+    levelPointsLeft = levelPointsLeft - 50;
+    luxuryBathroomClick = setInterval(clickOnGeorge, 1000, 5 * amountOfBathrooms);
   }
+  else{
+    alert("bathroom issues");
+  }
+}
+
+//adds more points to spend on powerups and levels the player to the next level
+function levelUp(){
+  levelPointsLeft = levelPointsLeft + (100 * level);
+  console.log(levelPointsLeft);
+  level++;
+  levelMult = levelMult + .5;
+  console.log("level: " + level);
+}
+
+
+//function that will display possible powerUps
+function availablePowerUps(){
+
 }
