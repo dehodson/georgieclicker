ff1f54fd04244800c686f597641ca90a0a84a269
Unknown <dehodson@gmail.com>
2016-11-15 22:00:26 -0500

    Updated game logic and updated CSS styles

--- georgie.js
+++ georgie.js
@@ -4,7 +4,7 @@ var totalClickAmount = 0;
 var luxuryBathroomClick;
 var georgeUpsetClick;
 var level = 1;
-var levelPointsLeft = 0;
+var playerCash = 0;
 var levelMult = 1;
 var amountOfBathrooms = 0;
 var angerLevel = 0;
@@ -16,10 +16,10 @@ function clickOnGeorge(clicks){
 		clicks = clickPower;
 	}
 	console.log(clicks);
-	console.log("points left :" + levelPointsLeft);
+	console.log("points left :" + playerCash);
 	totalClickAmount = totalClickAmount + clicks;
 	//checks to see if the player should level up because of this click
-	if(totalClickAmount - (Math.pow(100, levelMult)) >= 0){
+	if(totalClickAmount - (Math.pow(10, levelMult)) >= 0){
 		levelUp();
 		console.log("test");
 	}
@@ -32,10 +32,8 @@ function clickOnGeorge(clicks){
 //20 is the most you can have activated
 function luxuryBathroomSpots(amountOfBathroomsInput){
 	amountOfBathrooms = amountOfBathrooms + amountOfBathroomsInput;
-	if(amountOfBathrooms > 0 && amountOfBathrooms < 20 && levelPointsLeft >= 10){
-		clearInterval(luxuryBathroomClick);
-		levelPointsLeft = levelPointsLeft - 10;
-		luxuryBathroomClick = setInterval(clickOnGeorge, 1000, 5 * amountOfBathrooms);
+	if(amountOfBathrooms > 0 && amountOfBathrooms < 20 && playerCash >= 10){
+		spendCash(10);
 	}
 	else{
 		alert("bathroom issues");
@@ -43,28 +41,41 @@ function luxuryBathroomSpots(amountOfBathroomsInput){
 }
 function georgeUpset(angerLevelInput){
 	angerLevel = angerLevel + angerLevelInput;
-	if(angerLevel > 0 && angerLevel < 101 && levelPointsLeft >= 10 * angerLevel){
-		clearInterval(georgeUpsetClick);
-		levelPointsLeft = levelPointsLeft - (10 * angerLevel);
-		georgeUpsetClick = setInterval(clickOnGeorge, 1000, 10 * angerLevel);
+	if(angerLevel > 0 && angerLevel < 101 && playerCash >= 10 * angerLevel){
+		spendCash(10 * angerLevel);
 	}
 	else{
 		alert("nothing to be upset about");
 	}
 }
 
+function addCash(amount){
+	playerCash += amount;
+	document.getElementById("cash").innerText = playerCash;
+}
+
+function spendCash(amount){
+	playerCash -= amount;
+	document.getElementById("cash").innerText = playerCash;
+}
+
 //adds more points to spend on powerups and levels the player to the next level
 function levelUp(){
-	levelPointsLeft = levelPointsLeft + (100 * level);
-	console.log(levelPointsLeft);
+	addCash(100 * level);
+	console.log(playerCash);
 	level++;
 	levelMult = levelMult + .5;
 	console.log("level: " + level);
 	document.getElementById("level").innerText = level;
 }
 
+function gameTick(){
+	var clicksPerSecond = 0;
 
-//function that will display possible powerUps
-function availablePowerUps(){
+	clicksPerSecond +=  5 * amountOfBathrooms;
+	clicksPerSecond += 10 * angerLevel;
 
+	clickOnGeorge(clicksPerSecond);
 }
+
+setInterval(gameTick, 1000);
